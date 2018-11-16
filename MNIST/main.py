b3125d97ac4355e2d74ead655c02668c861a467f
Marius Anje <mariusanje@outlook.com>
2018-11-16 11:56:14 +0800

    update to pytorch 0.4

--- MNIST/main.py
+++ MNIST/main.py
@@ -8,13 +8,15 @@ import os
 import sys
 import models
 import util
+import tqdm
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
 import util
 
 def save_state(model, acc):
-    print('==> Saving model ...')
+    if args.verbose:
+        print('==> Saving model ...')
     state = {
             'acc': acc,
             'state_dict': model.state_dict(),
@@ -27,28 +29,31 @@ def save_state(model, acc):
 
 def train(epoch):
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data), Variable(target)
-        optimizer.zero_grad()
+    with tqdm.tqdm(enumerate(train_loader),total=len(train_loader),leave=False) as Loader:
+        tLoss = 0
+        for batch_idx, (data, target) in Loader:
+            if args.cuda:
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data), Variable(target)
+            optimizer.zero_grad()
 
-        # process the weights including binarization
-        bin_op.binarization()
+            # process the weights including binarization
+            bin_op.binarization()
 
-        output = model(data)
-        loss = criterion(output, target)
-        loss.backward()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            tLoss += loss.item()
+            Loader.set_description("Loss: %.4f"%(tLoss / (batch_idx + 1)))
 
-        # restore weights
-        bin_op.restore()
-        bin_op.updateBinaryGradWeight()
+            # restore weights
+            bin_op.restore()
+            bin_op.updateBinaryGradWeight()
 
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
+            optimizer.step()
+
+    print('Train Epoch: {}, Loss: {:.6f}'.format(
+        epoch, tLoss/len(train_loader)))
     return
 
 def test(evaluate=False):
@@ -58,34 +63,35 @@ def test(evaluate=False):
     correct = 0
 
     bin_op.binarization()
-    for data, target in test_loader:
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
-        output = model(data)
-        test_loss += criterion(output, target).data[0]
-        pred = output.data.max(1, keepdim=True)[1]
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+    with torch.no_grad():
+        for data, target in test_loader:
+            if args.cuda:
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data), Variable(target)
+            output = model(data)
+            test_loss += criterion(output, target).item()
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
     bin_op.restore()
     
-    acc = 100. * correct / len(test_loader.dataset)
+    acc = float(100 * correct) / float(len(test_loader.dataset))
     if (acc > best_acc):
         best_acc = acc
         if not evaluate:
             save_state(model, best_acc)
 
     test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
         test_loss * args.batch_size, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-    print('Best Accuracy: {:.2f}%\n'.format(best_acc))
+        float(100 * correct) / float(len(test_loader.dataset))))
+    print('Best Accuracy: {:.2f}%'.format(best_acc))
     return
 
 def adjust_learning_rate(optimizer, epoch):
     """Sets the learning rate to the initial LR decayed by 10 every 15 epochs"""
     lr = args.lr * (0.1 ** (epoch // args.lr_epochs))
-    print('Learning rate:', lr)
+    print('\nLearning rate:', lr)
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
     return lr
@@ -93,9 +99,9 @@ def adjust_learning_rate(optimizer, epoch):
 if __name__=='__main__':
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=512, metavar='N',
             help='input batch size for training (default: 128)')
-    parser.add_argument('--test-batch-size', type=int, default=128, metavar='N',
+    parser.add_argument('--test-batch-size', type=int, default=512, metavar='N',
             help='input batch size for testing (default: 128)')
     parser.add_argument('--epochs', type=int, default=60, metavar='N',
             help='number of epochs to train (default: 60)')
@@ -119,17 +125,20 @@ if __name__=='__main__':
             help='pretrained model')
     parser.add_argument('--evaluate', action='store_true', default=False,
             help='whether to run evaluation')
+    parser.add_argument('--verbose', action='store_true', default=False,
+            help='display more information')
     args = parser.parse_args()
     args.cuda = not args.no_cuda and torch.cuda.is_available()
 
-    print(args)
+    if args.verbose:
+        print(args)
     
     torch.manual_seed(args.seed)
     if args.cuda:
         torch.cuda.manual_seed(args.seed)
     
     # load data
-    kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+    kwargs = {'num_workers': 4, 'pin_memory': True} if args.cuda else {}
     train_loader = torch.utils.data.DataLoader(
             datasets.MNIST('data', train=True, download=True,
                 transform=transforms.Compose([
@@ -161,7 +170,8 @@ if __name__=='__main__':
     if args.cuda:
         model.cuda()
     
-    print(model)
+    if args.verbose:
+        print(model)
     param_dict = dict(model.named_parameters())
     params = []
     
